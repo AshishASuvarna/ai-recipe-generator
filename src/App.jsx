f5659beffbd80ca0ac9412161182e0f8b609df5e
Ashish Ananda Suvarna <ashish@Ashishs-MacBook-Air-2.local>
2025-07-13 07:11:12 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from "./Header"
+import Main from "./MainComponent"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeLogo from "./assets/chef.png"
+
+export default function Header() {
+    return (
+        <header>
+            <img src={chefClaudeLogo}/>
+            <h1>Chef Rordon Gamsey🥢</h1>
+        </header>
+    )
+}
